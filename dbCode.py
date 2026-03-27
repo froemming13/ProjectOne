4636dd14d0ca75acd150e4e02be8c5d9aad23f1a
froemming13 <mikaylafroemming@gmail.com>
2026-03-26 19:54:14 -0500

    Debugging code for get_all_students in dbCode.py

--- dbCode.py
+++ dbCode.py
@@ -43,9 +43,28 @@ def get_all_students():
     table = get_table()
     response = table.scan()
     items = response.get("Items", [])
-
-    if not items:
-        print("No students found.")
-        return
+    students = []
     
-    print(f"Here are {len(items)} student(s): ")
+    for item in response.get("Items", []):
+        student_id = int(item.get("student_id", {}).get("N", 0))
+        name = item.get("name", {}).get("S", "Unknown Name")
+        email = item.get("email", {}).get("S", "Unknown Email")
+        grade_level = int(item.get("grade_level", {}).get("N", 0))
+
+        #consulted with ChatGPT for assistance on this one due to DynamoDB table issues:
+        assignments = []
+        for a in item.get("assignments", {}).get("L", []):
+            am = a.get("M", {})
+            assignments.append({
+                "assignment_id": int(am.get("assignment_id",{}).get("N", 0)),
+                "title": am.get("title", {}).get("S", "Untitled"),
+                "score": int(am.get("score", {}).get("N", 0))})
+
+        students.append({
+            "student_id": student_id,
+            "name": name,
+            "email": email,
+            "grade_level": grade_level,
+            "assignments": assignments
+        })
+    return students
